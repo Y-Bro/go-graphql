8e9166facaf91463513af69fa438700ab0445642
Yerrumsetty Bharath <yerrumsetty.bharath@searce.com>
2022-11-29 15:31:51 +0530

    auth middleware

---
+++ internal/auth/middleware.go
@@ -0,0 +1,62 @@
+package auth
+
+import (
+	"context"
+	"net/http"
+	"strconv"
+
+	"github.com/Y-bro/go-graphql/internal/users"
+	"github.com/Y-bro/go-graphql/pkg/jwt"
+)
+
+type contextKey struct {
+	Name string
+}
+
+var userCtxKey = &contextKey{"user"}
+
+func Middleware() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+			header := r.Header.Get("Authorization")
+
+			if header == "" {
+				next.ServeHTTP(w, r)
+			}
+
+			tokenStr := header
+
+			username, err := jwt.ParseToken(tokenStr)
+
+			if err != nil {
+				http.Error(w, "Token invalid", http.StatusUnauthorized)
+				return
+			}
+
+			user := users.User{Username: username}
+
+			id, err := users.GetUserIdByUsername(username)
+
+			if err != nil {
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			user.ID = strconv.Itoa(id)
+
+			ctx := context.WithValue(r.Context(), userCtxKey, &user)
+
+			r = r.WithContext(ctx)
+
+			next.ServeHTTP(w, r)
+
+		})
+	}
+}
+
+func ForContext(ctx context.Context) *users.User {
+	raw, _ := ctx.Value(userCtxKey).(*users.User)
+
+	return raw
+}
